53dd8b790252367117d2ac3463e30a069f7be04d
Lasallec <134553613+Lasallec@users.noreply.github.com>
2024-04-25 16:32:02 -0400

    new message

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 
 	@GetMapping("/")
 	public String index() {
-		return "baconcheeseburger with fries please ban!";
+		return "baconcheeseburger with fries please!";
 	}
 	public ResponseEntity<String> healthCheck() {
 		return ResponseEntity.ok().build();
