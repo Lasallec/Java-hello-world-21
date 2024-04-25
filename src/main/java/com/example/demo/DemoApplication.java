609660d65295d75ad0977c3d7436a94d3d6c4563
Lasallec <134553613+Lasallec@users.noreply.github.com>
2024-04-25 13:23:56 -0400

    new message

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 
 	@GetMapping("/")
 	public String index() {
-		return "what up SpringBoot 23!";
+		return "what up SpringBoot 233!";
 	}
 	public ResponseEntity<String> healthCheck() {
 		return ResponseEntity.ok().build();
